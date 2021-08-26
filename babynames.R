56b1874f0f0e4c2f4d5a76e5e63bf47daa178615
Joe DeCesaro <jdecesaro@bren.ucsb.edu>
2021-08-26 13:59:42 -0700

    Updated the code to include comments that explain what the functions are doing

--- babynames.R
+++ babynames.R
@@ -3,14 +3,23 @@ library(tidyverse)
 babynames <- read.csv("data/CA.TXT", header = FALSE)
 colnames(babynames) <- c("state", "gender", "year", "firstname", "count")
 head(babynames)
-juliens <- babynames%>%
-  filter(firstname == "Julien")
+# The above section of code reads in the baby name data for California and names the columns in the order of the created vector.
 
-famousJuliens <- max(juliens$count)
 
-best_julien <- juliens %>% 
-  filter (count == famousJuliens) %>%
-  select(year) %>% pull()
-best_julien
+joes <- babynames%>%
+  filter(firstname == "Joe")
+# This code filters the baby name data set previously created to only keep the name "Joe"
 
-juliens[juliens$year > 2010,]
+
+famousJoes <- max(joes$count)
+# This line makes a variable that finds the highest count in all the years of the joes data.
+
+best_joe<- joes %>% 
+  filter (count == famousJoes) %>%
+  select(year) %>% 
+  pull()
+best_joe
+# This pipe sequence creates finds the year with the most names "joe" in the data set by looking for the matching year that corresponds with the max count found in "famousJoes"
+
+joes[joes$year > 2010,]
+# This looks into the joes data set and asks to return only the years greater than 2010.
